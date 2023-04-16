174e414b1f5f315c46d9eb8450b7f9995ed8731a
JCKalt <ckaltwasser@reminingcorp.com>
2023-04-16 13:39:47 -0400

    fixed warnings and tests pass

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -1,7 +1,5 @@
-// Term enum represents the basic building blocks of a Prolog program, including atoms, variables, and structures. 
-// The Structure variant has a functor (name), arity (number of arguments), and a vector of arguments.
-// The Clause struct represents a Prolog clause, which consists of a head and a body. The head is a single term,
-//  and the body is a vector of terms.
+// src/parser/ast.rs
+use crate::parser::lexer::tokenize;
 
 pub enum Term {
     Atom(String),
@@ -17,8 +15,6 @@ pub struct Clause {
     pub head: Term,
     pub body: Vec<Term>,
 }
-
-use crate::parser::lexer::{Token, tokenize};
 
 pub type ParseResult<T> = Result<T, ParseError>;
 
@@ -40,4 +36,3 @@ impl From<crate::parser::lexer::LexerError> for ParseError {
         ParseError::LexerError(error)
     }
 }
-
